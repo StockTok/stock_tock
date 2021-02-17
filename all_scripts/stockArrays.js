4801be24a68ee2380145c94e4b303feb10c61b2d
Peter Shin <pshin2@masonlive.gmu.edu>
2021-02-17 18:23:29 -0500

    stockArray.js
    added dummy functions as a placeholder

--- all_scripts/stockArrays.js
+++ all_scripts/stockArrays.js
@@ -8,3 +8,25 @@ const stocks = ['aapl', 'msft', 'amzn', 'fb', 'googl', 'tsla', 'goog', 'brk.b', 
 'bmy', 'wfc', 'pm', 'lin', 'c', 'low', 'sbux', 'orcl', 'ups', 'now', 'ba', 'amd', 'rtx', 'cat', 'ibm', 'intu', 'ms', 'gs', 'amat',
 'blk', 'mmm', 'amt', 'ge', 'de', 'cvs', 'mu', 'tgt', 'isrg', 'chtr', 'gild', 'fis', 'axp', 'lmt', 'schw', 'tjx', 'lrcx', 'spgi',
 'mo', 'syk', 'atvi', 'pld', 'mdlz', 'zts', 'ci', 'cb', 'bdx', 'antm', 'tmus'];
+
+let tempStocks;
+
+
+reset();
+
+
+// Resets the stack, basically it creates a shallow copy of the stocks list
+function reset() {
+    tempStocks = [...stocks];
+}
+
+// Will add stock to followedStocks array
+// Will also delete from shallow copy
+function swipeLeft() {
+    
+}
+
+// Will delete from shallow copy
+function swipeRight() {
+
+}
