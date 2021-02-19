399af99b961542ecf8789e7dcaa9477fa308cc9a
jaudick <jaaudick@gmail.com>
2021-02-19 11:50:39 -0500

    dictionary references

--- all_scripts/newsAPI.js
+++ all_scripts/newsAPI.js
@@ -1,9 +1,12 @@
 const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('2bc6b32f847f4361ab90a79c93fe9474');
-
+const { symbol, name } = require('./stockDictionary.js');
+/*
 newsapi.v2.everything({
-    q: 'Microsoft',
+    q: 'microsoft',
     language: 'en'
   }).then(response => {
     console.log(response);
-});
+});*/
+
+console.log(name);
