ba0ad165aa5155d063cb8cefefa4a7e97d16edeb
ttran92 <v9tran@gmail.com>
2021-02-19 13:23:02 -0500

    finished with the deletePrompt() in deleteFromWatchlist.js

--- all_scripts/deleteFromWatchlist.js
+++ all_scripts/deleteFromWatchlist.js
@@ -1,3 +1,4 @@
+const { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } = require('constants');
 let fs = require('fs') //filesystem
 
 const prompt = require('prompt');
@@ -7,24 +8,29 @@ const jsonOutput = {'following' : [], 'notFollowing' : []};
 const followedStocks = [];
 const notFollowedStocks = [];
 
+
+//Allows the user to type in what stock they want to delete.
 function deletePrompt(){
-    console.log('type in a stock to delete');
+    console.log('Type in a stock to delete');
     prompt.get(['option'], (err,result) =>{
         if(err)
         throw err;
+        console.log(`The user entered: ${result.option}`);
+        deleteStock(result.option);
+    });
 
-        console.log(result.option);
-    })
+
 }
-function deleteStock(stockToDelete)
-{
-    var file = JSON.parse(fs.readFileSync('stockDataDelete.json', 'utf-8'));
-    console.log(file.following);
-
-    let JSONdata = JSON.stringify(jsonOutput);
-    fs.writeFileSync('stockData.json', JSONdata);
-}
-
 
 deletePrompt();
-deleteStock();
+
+function deleteStock(stockToDelete) {
+      var file = JSON.parse(fs.readFileSync('stockDataDelete.json', 'utf-8'));
+      console.log(file.following);
+
+      let JSONdata = JSON.stringify(jsonOutput);
+      fs.writeFileSync('stockData.json', JSONdata);
+    }
+
+
+
