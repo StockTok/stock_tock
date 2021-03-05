015088ab7db0615b7597dd5ccebfb001907fc7d2
jaudick <jaaudick@gmail.com>
2021-03-05 10:48:54 -0500

    update1

--- all_scripts/back4app.js
+++ all_scripts/back4app.js
@@ -5,8 +5,12 @@ Parse.serverURL = 'https://parseapi.back4app.com/'
 /*
 const Stocks = Parse.Object.extend("Stocks");
 const stock = new Stocks();
+stock.set("playerName", "A. Wed");
+stock.set("yearOfBirth", 1997);
+stock.set("emailContact", "a.wed@email.io");
+stock.set("attributes", ["fast","good conditioning"])
 
-stock.set("TestName", "This is a test name");
+stock.set("TestName", "Great Name");
 stock.save()
 .then((object) => {
     console.log(object.id);
@@ -20,9 +24,29 @@ const stock  = new Parse.Query(Stocks);
 
 stock.get("egwKw5fCK7")
 .then((stock) => {
+    stock.set("TestName", "Willy Stevens");
     console.log(stock.get('TestName'));
+    stock.save();
   // The object was retrieved successfully.
 }, (error) => {
     console.log(error.message);
   // The object was not retrieved successfully.
 });
+/*
+const Stock = Parse.Object.extend("Stocks");
+const soccerPlayers = new Parse.Query(Stock);
+
+// Retrieve the object by id
+soccerPlayers.get("Nano0VByLI")
+.then((player) => {
+  // The object was retrieved successfully and it is ready to update.
+  player.destroy().then((player) => {
+      console.log(player.id);
+    // The object was deleted from the Parse Cloud.
+  }, (error) => {
+    // The delete failed.
+    // error is a Parse.Error with an error code and message.
+  })
+}, (error) => {
+  // The object was not retrieved successfully.
+});*/
