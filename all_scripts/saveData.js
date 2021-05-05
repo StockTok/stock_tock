4809260b7cdd5c17584a081c65e5ea8cef829322
jaudick <jaaudick@gmail.com>
2021-05-04 20:59:28 -0400

    Update saveData.js

--- all_scripts/saveData.js
+++ all_scripts/saveData.js
@@ -3,7 +3,7 @@ var Parse = require('parse/node');
 Parse.initialize("jiM3dxKMrJoyJ3OFSOvKjkNVlWCfJ3GsNknSuqsf","cuRUV83XrqhpyKKMzc5UnHTWxQLmcQSA7lDjSx6N"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
 Parse.serverURL = 'https://parseapi.back4app.com/'
 
-const saveData = async (username, followed) => {
+const saveAllData = async (username, followed) => {
 
 const Account = Parse.Object.extend("Account");
 const query = new Parse.Query(Account);
