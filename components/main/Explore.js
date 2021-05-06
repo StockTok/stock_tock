91837d1a6544abf3233d13f5aaecf80c4025400b
jaudick <jaaudick@gmail.com>
2021-05-05 23:40:02 -0400

    Revert "design changes made"
    This reverts commit 0fdfdcc5e05156222857eb8877f2b64e97fa3881.

--- components/main/Explore.js
+++ components/main/Explore.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { StyleSheet, Text, Image, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import CardStack, { Card } from "react-native-card-stack-swiper";
-import { Deck, ListItem, Button, Icon } from 'react-native-elements'
 import Watchlist from "./Watchlist.js";
 // import stocks from '../test.json';
 
 export default function Explore() {
-  
   return (
     <View style={styles.container}>
       <CardStack
@@ -19,23 +17,23 @@ export default function Explore() {
         ref={(swiper) => {
           swiper;
         }}
-        onSwiped={() => console.log("right swipe")}
-        onSwipedLeft={() => console.log("left swipe")}
+        onSwiped={() => console.log("onSwiped")}
+        onSwipedLeft={() => console.log("onSwipedLeft")}
       >
         <Card style={[styles.card]}>
-          <Text style={styles.label}>amzn</Text>
+          <Text style={styles.label}>A</Text>
         </Card>
         <Card style={[styles.card]}>
-          <Text style={styles.label}>appl</Text>
+          <Text style={styles.label}>B</Text>
         </Card>
         <Card style={[styles.card]}>
-          <Text style={styles.label}>googl</Text>
+          <Text style={styles.label}>C</Text>
         </Card>
         <Card style={[styles.card]}>
-          <Text style={styles.label}>msft</Text>
+          <Text style={styles.label}>D</Text>
         </Card>
         <Card style={[styles.card]}>
-          <Text style={styles.label}>tsla</Text>
+          <Text style={styles.label}>E</Text>
         </Card>
       </CardStack>
     </View>
@@ -46,7 +44,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
-    backgroundColor: "#2f3640",
+    backgroundColor: "#ebf5fc",
   },
   content: {
     flex: 5,
@@ -56,7 +54,7 @@ const styles = StyleSheet.create({
   card: {
     width: 320,
     height: 470,
-    backgroundColor: "#dcdde1",
+    backgroundColor: "#FE474C",
     borderRadius: 5,
     shadowColor: "rgba(0,0,0,0.5)",
     shadowOffset: {
@@ -65,14 +63,13 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.2,
   },
-   label: {
-    // loader: "json-loader",
-     lineHeight: 450,
-     textAlign: "center",
-     fontSize: 55,
-     fontFamily: "System",
-     color: "#2f3640",
-     backgroundColor: "transparent",
-     textTransform: 'uppercase'
-   },
+  // label: {
+  //   loader: "json-loader",
+  //   lineHeight: 400,
+  //   textAlign: "center",
+  //   fontSize: 55,
+  //   fontFamily: "System",
+  //   color: "#ffffff",
+  //   backgroundColor: "transparent",
+  // },
 });
