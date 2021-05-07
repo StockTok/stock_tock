4f5108e925395b0326d12fbcfeb0b537f9f36399
Abraham1361 <78248915+Abraham1361@users.noreply.github.com>
2021-05-07 19:47:21 -0400

    we made changes to explore, all we did was set the stock dictionary to grab the users followed data

--- components/main/Explore.js
+++ components/main/Explore.js
@@ -2,67 +2,95 @@ import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import CardStack, { Card } from "react-native-card-stack-swiper";
 import { getAllDataMethod } from "../../all_scripts/getAllData.js";
-import { shuffleArticles } from "../../all_scripts/shuffleNews.js";
+import { saveAllData } from "../../all_scripts/saveData.js";
+
+// import { shuffleArticles } from "../../all_scripts/shuffleNews.js";
 GLOBAL = require("../GlobalState.js");
-
-
-
+var proof = 0;
 export default function Explore() {
-
+  
+  const [userDictionary, setUserDictionary] = useState(null);
   const [newsArray, setNewsArray] = useState([]);
   const [loadedNewsArray, setLoadedNewsArray] = useState(false);
+  const [followedArray, setFollowedArray] = useState([]);
+  const [userObject, setUserObject] = useState(null);
+  const [title, setTitle] = useState("news");
+  const [count, setCount] = useState(0);
 
+  /*
+  we need to keep the double await so dont delete it
+  we fixed the shuffled articles to contain the news stuff
+  */
+  useEffect(() => {
+    async function getMyData() {
+      //await getAllStockData();
+      let userData = await getAllDataMethod(GLOBAL.USERNAME);
+        userData = await getAllDataMethod(GLOBAL.USERNAME);// we need this double
+        maxi++;
+        setUserObject(userData);
+        setFollowedArray(userData.followed);
+        setUserDictionary(userData.stocks);
+        try{
+          let shuffledArticles = [];
+          if(userData.followed.length > 0) {   //we ditched the followed array idea
+            for(let i = 0; i < userData.followed.length ; i++) {
+              shuffledArticles.push(...userData.stocks[userData.followed[i]]["news"]);
+            }
+            shuffledArticles = shuffle(shuffledArticles);
+            setNewsArray(shuffledArticles);
+          } 
+        }
+        catch(error) {
+          console.log(error);
+        }
 
-  //useEffect(() => {
-   // console.log("hello world");
-   // async function getMyData() {
-    //  let userData = await getAllDataMethod(GLOBAL.USERNAME);
-    //  console.log(userData);
-    //  console.log("inside getMyData " + userData.followed);
-    //  setStockUserArray(userData.followed);
-    //  setStockDictionary(userData.stocks);
-    //  var newArray = await shuffleArticles(userData);
-     // console.log("newsArray called"+newArray);
-    //  setNewsArray(newArray);
-//}
-  //  console.log("loaded? " + loadedNewsArray);
-  //  if (loadedNewsArray === false) {
- //     console.log("call to database");
+    }
+    if (loadedNewsArray === false || newsArray.length === 0) {
+      console.log("call to database");
       //anytime edits are made we need to setLoadedArray(false) to ensure a reload of the correct elements
-  //    setLoadedNewsArray(true);
-  //    getMyData();
-  //  }
- // }, []);
+      setLoadedNewsArray(true);
+      getMyData();
+    }
+    console.log("hello world"); 
+    console.log("loaded? " + loadedNewsArray);
 
-  console.log(newsArray);
+    function shuffle(a) {
+      var j, x, i;
+      for (i = a.length - 1; i > 0; i--) {
+          j = Math.floor(Math.random() * (i + 1));
+          x = a[i];
+          a[i] = a[j];
+          a[j] = x;
+      }
+      return a;
+    }
+
+    const swipeNews = () => {
+      setTitle(newsArray[count]["title"]);
+      console.log("SWIPER NO SWIPING" + newsArray[count]["title"]);
+      console.log(title);
+      setCount(count+1);
+    }
+
+  }, []);
+
 
   return (
     <View style={styles.container}>
       <CardStack
         style={styles.content}
+        loop = {true}
         renderNoMoreCards={() => (
           <Text style={styles.nomoreText}>No more cards</Text>
         )}
         ref={(swiper) => {
           swiper;
         }}
-        onSwiped={() => console.log("onSwiped")}
-        onSwipedLeft={() => console.log("onSwipedLeft")}
+        onSwipeStart={() => proof++}
+        // onSwipedLeft={() => swipeNews()}
       >
         <Card style={[styles.card]}>
-          <Text style={styles.label}>a</Text>
-        </Card>
-        <Card style={[styles.card]}>
-          <Text style={styles.label}>b</Text>
-        </Card>
-        <Card style={[styles.card]}>
-          <Text style={styles.label}>c</Text>
-        </Card>
-        <Card style={[styles.card]}>
-          <Text style={styles.label}>D</Text>
-        </Card>
-        <Card style={[styles.card]}>
-          <Text style={styles.label}>E</Text>
+          <Text style={styles.label}>{newsArray}</Text>
         </Card>
       </CardStack>
     </View>
